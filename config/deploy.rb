34781ca95e75452ae8e95f6e44919663b3410679
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-08-08 09:30:57 +0900

    deploy.rbの構成を修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,50 +12,62 @@ set :user, "ubuntu"
 set :password, "ubuntu"
 set :use_sudo, false 
 
-#role :web, "192.168.56.101"                          # your http server, apache/etc
-#role :app, "192.168.56.101"                          # this may be the same as your `web` server
-#role :db,  "192.168.56.101", :primary => true # this is where rails migrations will run
-#role :db,  "your slave db-server here"
+# roles
+role :web, "192.168.56.101"                          # Your HTTP server, Apache/etc
+role :db,  "192.168.56.101", :primary => true # This is where Rails migrations will run
+role :memcache, "192.168.56.101"
+set :deploy_to, "/home/ubuntu/www/#{application}"
 
-#set :deploy_to, "/home/ubuntu/www/#{application}"
-
-task :dev do
-	# roles
-	role :web, "192.168.56.101"                          # Your HTTP server, Apache/etc
-	role :db,  "192.168.56.101", :primary => true # This is where Rails migrations will run
-	role :memcache, "192.168.56.101"
-
-	set :deploy_to, "/home/ubuntu/www/#{application}"
-	common_task
-end
-
-task :common_task do
-
-	namespace :deploy do
-		task :start, :roles => :web do
-			run "apache2ctl configtest"
-			sudo "service apache2 restart"
-		end
-
-		task :stop, :roles => :web do
-			sudo "service apache2 stop"
-		end
+namespace :deploy do
+	task :start, :roles => :web do
+		run "apache2ctl configtest"
+		sudo "service apache2 restart"
 	end
 
-	namespace :db do
-		set :db_service, "mysql"
-		task :start, :roles => :db do
-			sudo "service #{db_service} restart"
-		end
-		task :stop, :roles => :db do
-			sudo "service #{db_service} stop"
-		end
+	task :stop, :roles => :web do
+		sudo "service apache2 stop"
 	end
 
-	namespace :memcache do
-		task :start, :roles => :memcache do
-			sudo "service memcached restart"
-		end
+	task :restart, :roles => :web do
+		run "apache2ctl configtest"
+		sudo "service apache2 restart"
+	end
+
+	task :finalize_update, :except => { :no_release => true } do
+		diff_source
 	end
 
 end
+
+namespace :db do
+	set :db_service, "mysql"
+	task :start, :roles => :db do
+		sudo "service #{db_service} restart"
+	end
+
+	task :stop, :roles => :db do
+		sudo "service #{db_service} stop"
+	end
+end
+
+namespace :memcache do
+	task :start, :roles => :memcache do
+		sudo "service memcached restart"
+	end
+end
+
+def diff_source
+	set :diff_exclude, ".git"
+	run <<-CMD
+		echo 'finalize_update';
+		localdiff="$(diff -ru --exclude=#{diff_exclude} #{previous_release} #{latest_release})";
+		if [ "$localdiff" != "" ]; then
+			echo '========NOT SAME SOURCE========';
+			echo "$localdiff";
+	    	exit 1;
+		else
+			echo '========SAME SOURCE========';
+		fi
+	CMD
+end
+         
